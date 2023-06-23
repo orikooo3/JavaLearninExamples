caea15d9bd8b5d1736e448668023a4d725495668
orikopg <131629915+orikopg>
2023-06-23 10:44:04 +0900

    first commit

---
+++ BasicCode.java
@@ -0,0 +1,25 @@
+package com.example.learning.chapter1.code1;
+
+public class BasicCode {
+    public static void main(String[] args) {
+        // 変数の宣言と初期化
+        int number1 = 5;
+        int number2 = 10;
+
+        // 足し算の結果を表示
+        int sum = number1 + number2;
+        System.out.println("Sum: " + sum);
+
+        // 引き算の結果を表示
+        int difference = number2 - number1;
+        System.out.println("Difference: " + difference);
+
+        // 掛け算の結果を表示
+        int product = number1 * number2;
+        System.out.println("Product: " + product);
+
+        // 割り算の結果を表示
+        int quotient = number2 / number1;
+        System.out.println("Quotient: " + quotient);
+    }
+}

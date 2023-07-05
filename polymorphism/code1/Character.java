c5c1c420b48185a2733a793edd44567c8e468a1b
orikopg <ori.kou.1025@gmail.com>
2023-07-05 23:05:17 +0900

    polymorphismSample1 commit

---
+++ polymorphism/code1/Character.java
@@ -0,0 +1,5 @@
+package com.example.learning.polymorphism.code1;
+
+public class Character {
+
+}
---
+++ polymorphism/code1/Main.java
@@ -0,0 +1,9 @@
+package com.example.learning.polymorphism.code1;
+
+public class Main {
+    public static void main(String[] args) {
+        Character c1 = new Hero();
+        Hero h = (Hero) c1;
+
+    }
+}

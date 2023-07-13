e2670a002c871fa4a95e659a8a73781b969029dd
orikopg <ori.kou.1025@gmail.com>
2023-07-13 10:14:53 +0900

    カプセル化例題

---
+++ encapsulation/ex1/Wizard.java
@@ -0,0 +1,45 @@
+package com.example.learning.encapsulation.ex1;
+
+public class Wizard {
+
+    private int hp;
+    private int mp;
+    private String name;
+    private Wand wand;
+
+    public void heal() {
+
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getMp() {
+        return mp;
+    }
+
+    public void setMp(int mp) {
+        this.mp = mp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Wand getWand() {
+        return wand;
+    }
+
+    public void setWand(Wand wand) {
+        this.wand = wand;
+    }
+}

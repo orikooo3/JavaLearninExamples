56dd5e88d9f6882f68f4a72489b6913afe41e39d
orikopg <ori.kou.1025@gmail.com>
2023-07-20 22:54:06 +0900

    constructorSamplecode3 commit

---
+++ chapter9/code3/SubClass.java
@@ -0,0 +1,13 @@
+package com.example.learning.chapter9.code3;
+
+public class SubClass extends SuperClass {
+
+    public SubClass() {
+        super();
+
+    }
+
+    public SubClass(int num) {
+        this();
+    }
+}
---
+++ chapter9/code3/SuperClass.java
@@ -0,0 +1,12 @@
+package com.example.learning.chapter9.code3;
+
+public class SuperClass {
+
+    public SuperClass(int num, String str) {
+
+    }
+
+    public SuperClass() {
+        this(1, "1");
+    }
+}

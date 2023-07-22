81fd6e647c9cc213189ef90ccb29ab33b7ce4672
orikopg <ori.kou.1025@gmail.com>
2023-07-22 21:02:51 +0900

    文字列処理サンプルコード1

---
+++ stringdate/code1/Main.java
@@ -0,0 +1,25 @@
+package com.example.learning.stringdate.code1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s1 = "  　　　";
+        String s2 = "Java";
+        String s3 = "java";
+
+        if (s2.equals(s3)) {
+            System.out.println("s2とs3は等しい");
+        }
+
+        if (s2.equalsIgnoreCase(s3)) {
+            System.out.println("s2とs3はケースを区別しなければ等しい");
+        } else {
+            System.out.println("s2とs3は等は等しくない");
+        }
+        System.out.println("s1の長さは" + s1.length() + "です");
+
+        if (s1.isEmpty()) {
+            System.out.println("s1は空白文字です");
+        }
+    }
+}
